80cfd901e086b3908e85a753318a230b183cbaca
contradiction29 <sora32127@gmail.com>
2024-08-06 17:18:25 +0900

    Add Metafunction

--- app/routes/_layout.about.tsx
+++ app/routes/_layout.about.tsx
@@ -1,5 +1,5 @@
 import { H1, H2 } from "~/Components/Headings";
-import { NavLink } from "@remix-run/react";
+import { MetaFunction, NavLink } from "@remix-run/react";
 
 export default function About() {
     return (
@@ -14,3 +14,26 @@ export default function About() {
       </div>
     )
 } 
+
+export const meta: MetaFunction = () => {
+  const title = "現実モデリング - About";
+  const description = "contradiction29の個人ブログだよ";
+  const defaultOGImageURL = "https://x.com/contradiction29/photo"
+  return [
+    { title },
+    { description },
+    { property: "og:title", content: title },
+    { property: "og:description", content: description },
+    { property: "og:locale", content: "ja_JP" },
+    { property: "og:site_name", content: "現実モデリング" },
+    { property: "og:type", content: "website" },
+    { property: "og:url", content: "https://contradictionononline.com" },
+    { property: "og:image", content: defaultOGImageURL },
+    { name: "twitter:card", content: "summary_large_image" },
+    { name: "twitter:site", content: "https://x.com/contradiction29" },
+    { name: "twitter:title", content: title },
+    { name: "twitter:description", content: description },
+    { name: "twitter:creator", content: "https://x.com/contradiction29" },
+    { name: "twitter:image", content: defaultOGImageURL },
+  ];
+};
--- app/routes/_layout.recents.tsx
+++ app/routes/_layout.recents.tsx
@@ -1,4 +1,4 @@
-import { json, LoaderFunctionArgs } from "@remix-run/cloudflare";
+import { json, LoaderFunctionArgs, MetaFunction } from "@remix-run/cloudflare";
 import { useLoaderData } from "@remix-run/react";
 import { H1, H2 } from "~/Components/Headings";
 import { PostShowCard } from "~/Components/PostShowCard";
@@ -26,3 +26,26 @@ export default function Recents() {
         </div>
     );
 }
+
+export const meta: MetaFunction = () => {
+    const title = "現実モデリング";
+    const description = "contradiction29の個人ブログだよ";
+    const defaultOGImageURL = "https://x.com/contradiction29/photo"
+    return [
+      { title },
+      { description },
+      { property: "og:title", content: title },
+      { property: "og:description", content: description },
+      { property: "og:locale", content: "ja_JP" },
+      { property: "og:site_name", content: "現実モデリング" },
+      { property: "og:type", content: "website" },
+      { property: "og:url", content: "https://contradictionononline.com" },
+      { property: "og:image", content: defaultOGImageURL },
+      { name: "twitter:card", content: "summary_large_image" },
+      { name: "twitter:site", content: "https://x.com/contradiction29" },
+      { name: "twitter:title", content: title },
+      { name: "twitter:description", content: description },
+      { name: "twitter:creator", content: "https://x.com/contradiction29" },
+      { name: "twitter:image", content: defaultOGImageURL },
+    ];
+  };
