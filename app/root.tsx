99cedf6add095036821eebc816617ebb963e4085
contradiction29 <sora32127@gmail.com>
2024-08-07 09:32:42 +0900

    Add 404 page

--- app/root.tsx
+++ app/root.tsx
@@ -1,14 +1,18 @@
 import {
+  isRouteErrorResponse,
   Links,
   Meta,
+  NavLink,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useRouteError,
 } from "@remix-run/react";
 import "./tailwind.css";
 import { PageTransitionProgressBar } from "./Components/PageTransitionProgressBar";
 
 import 'highlight.js/styles/tokyo-night-dark.css';
+import { H1 } from "./Components/Headings";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -41,6 +45,27 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </body>
     </html>
   );
+}
+
+export function ErrorBoundary(){
+  const error = useRouteError();
+  if (isRouteErrorResponse(error) && error.status === 404){
+    return (
+      <div className="flex flex-col items-center justify-center h-screen">
+        <H1>404 Page Not Found</H1>
+        <p>お探しのページは見つかりませんでした。</p>
+        <NavLink to="/" className="btn btn-primary">トップページに戻る</NavLink>
+      </div>
+    )
+  } else {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen">
+        <H1>500 Internal Server Error</H1>
+        <pre className="bg-error p-4 mb-8 overflow-auto">{error instanceof Error ? error.stack : JSON.stringify(error)}</pre>
+        <NavLink to="/" className="btn btn-primary">トップページに戻る</NavLink>
+      </div>
+    )
+  }
 }
 
 export default function App() {
